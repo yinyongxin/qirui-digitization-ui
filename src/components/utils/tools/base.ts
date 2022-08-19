16e05e1c7a5a696b5909b1e403199f6c1cb08373
银永鑫 <1101278643@qq.com>
2022-08-19 19:21:03 +0800

    getValueFromObjectByString

--- src/components/utils/tools/base.ts
+++ src/components/utils/tools/base.ts
@@ -13,3 +13,20 @@ export const getValueIfQualified = <O = unknown>(value: O, condition: boolean | 
     }
   }
 }
+
+/**
+ * 从对象中根据字符串获取值
+ * @param initialValues 对象
+ * @param fieldsString 
+ * @returns 
+ */
+export const getValueFromObjectByString = (initialValues: Record<string, any>, fieldsString: string) => {
+  const fieldNameArr = fieldsString?.split('.') || []
+  const fieldNameLength = fieldNameArr.length
+  let value = initialValues
+  for (let index = 0; index < fieldNameLength; index++) {
+    const fieldName = isNaN(Number(fieldNameArr[index])) ? fieldNameArr[index] : Number(fieldNameArr[index])
+    value = value[fieldName]
+  }
+  return value
+}
