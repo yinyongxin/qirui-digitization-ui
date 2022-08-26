0cfc2d8b91acfcdf8c5df04e0b7ad34184f4daf5
银永鑫 <1101278643@qq.com>
2022-08-26 14:54:27 +0800

    form store

---
+++ src/components/Form/Context.tsx
@@ -0,0 +1,20 @@
+import { createContext } from "react";
+import { FormContextType, FormItemContextType } from "./interface";
+export const FormContextDefult: FormContextType = {
+  layout: 'horizontal',
+  labelAlign: 'right',
+  requiredSymbol: true,
+  columns: 1,
+  inForm: false
+}
+
+export const FormContext = createContext<FormContextType>(FormContextDefult)
+FormContext.displayName = 'FormContext'
+
+
+export const FormItemContext = createContext<FormItemContextType>({
+  name: '',
+  inFormItem: false,
+  validateStatus: 'error'
+});
+FormItemContext.displayName = 'FormItemContext'
